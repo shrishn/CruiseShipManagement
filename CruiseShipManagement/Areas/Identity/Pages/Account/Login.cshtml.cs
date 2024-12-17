2f0a02161a9746ad8b5dca4ea30dfdf2043220c9
Shrish Nandakumar <2370018@cognizant.com>
2024-12-17 18:02:35 +0530

    login error fixed

--- CruiseShipManagement/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CruiseShipManagement/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -112,20 +112,32 @@ namespace CruiseShipManagement.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
+                var user = await _signInManager.UserManager.FindByEmailAsync(Input.Email);
+
+                if (user != null)
                 {
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
+                    var result = await _signInManager.PasswordSignInAsync(
+                        user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("User logged in.");
+                        return LocalRedirect(returnUrl);
+                    }
+                    else if (result.RequiresTwoFactor)
+                    {
+                        return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    }
+                    else if (result.IsLockedOut)
+                    {
+                        _logger.LogWarning("User account locked out.");
+                        return RedirectToPage("./Lockout");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return Page();
+                    }
                 }
                 else
                 {
---
+++ CruiseShipManagement/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using CruiseShipManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace CruiseShipManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> TestPassword()
+        {
+            var admin = await _userManager.FindByEmailAsync("admin@example.com");
+            if (admin == null)
+            {
+                return Content("Admin user not found.");
+            }
+
+            var isPasswordCorrect = await _userManager.CheckPasswordAsync(admin, "Admin@123");
+            if (isPasswordCorrect)
+            {
+                return Content("Password is correct.");
+            }
+            else
+            {
+                return Content("Password is incorrect.");
+            }
+        }
+    }
+}
